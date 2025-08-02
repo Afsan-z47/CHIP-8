8985e1c66339781ce5246ba6320d1926ab52fe21
Afsan-z47 <ns.afsan@proton.me>
2025-08-02 10:11:58 +0600

    Completion on Display OPCODEs

--- src/chip8.c
+++ src/chip8.c
@@ -63,7 +63,7 @@ CHIP_8 init_EMU(FILE *ROM) {
 	init_GPR(EMULATOR.GPR);
 	init_STACK(EMULATOR.STACK);
 
-	EMULATOR.OPCODE = (EMULATOR.MEMORY[0x200] << 8) | EMULATOR.MEMORY[0x201];
+	EMULATOR.OPCODE = 0;
 	EMULATOR.SP  = 0;
 	EMULATOR.PC = 0x200;
 
@@ -73,6 +73,47 @@ CHIP_8 init_EMU(FILE *ROM) {
 
 	return EMULATOR;
 }
+
+
+// NOTE: LIST OF OPCODES AND THEIR PROCESSESS
+// [x] 00E0     Clear display 			
+// 00EE     Return from subroutine
+// [x] 1NNN     Jump to NNN 
+// [x] 2NNN     Call subroutine at NNN 
+// 3XKK     Skip next instruction if VX == KK 
+// 4XKK     Skip next instruction if VX <> KK 
+// 5XY0     Skip next instruction if VX == VY 
+// [x] 6XKK     VX := KK 
+// [x] 7XKK     VX := VX + KK 
+// 8XY0     VX := VY, VF may change 
+// 8XY1     VX := VX or VY, VF may change 
+// 8XY2     VX := VX and VY, VF may change 
+// 8XY3     VX := VX xor VY, VF may change
+// [-] 8XY4     VX := VX + VY, VF := carry 
+// 8XY5     VX := VX - VY, VF := not borrow 
+// 8XY6     VX := VX shr 1, VF := carry 
+// 8XY7     VX := VY - VX, VF := not borrow
+// 8XYE     VX := VX shl 1, VF := carry 
+// 9XY0     Skip next instruction if VX <> VY 
+// [x] ANNN     I := NNN 
+// BNNN     Jump to NNN+V0 
+// CXKK     VX := pseudorandom_number and KK 
+//          collision. If N=0 and extended mode, show 16x16 sprite.
+// [x] DXYN    Show N-byte sprite from M(I) at coords (VX,VY), VF :=
+//          collision. If N=0 and extended mode, show 16x16 sprite.
+// [x] EX9E     Skip next instruction if key VX pressed 
+// EXA1     Skip next instruction if key VX not pressed 
+// FX07     VX := delay_timer 
+// FX0A     wait for keypress, store hex value of key in VX 
+// FX15     delay_timer := VX 
+// FX18     sound_timer := VX 
+// FX1E     I := I + VX 
+// FX29     Point I to 5-byte font sprite for hex character VX 
+// FX33     Store BCD representation of VX in M(I)..M(I+2) 
+// FX55     Store V0..VX in memory starting at M(I) 
+// FX65     Read V0..VX from memory starting at M(I)
+
+
 
 
 void emulateCycle(CHIP_8 *chip) {
@@ -95,7 +136,7 @@ void emulateCycle(CHIP_8 *chip) {
 			switch(chip->OPCODE & 0x000F)
 			{
 				case 0x0000: // 0x00E0: Clears the screen        
-					// Execute opcode
+					memset(chip->DISPLAY, 0 , 64*32);
 					break;
 
 				case 0x000E: // 0x00EE: Returns from subroutine          
@@ -107,15 +148,34 @@ void emulateCycle(CHIP_8 *chip) {
 			}
 			break;
 
+		case 0x1000: // 1NNN: Jump to NNN
+			// FIXME: LOOK for some increment problem here
+			// I did not send back pc by 2
+			// After testing this part will be decided
+			chip->PC = chip->OPCODE & 0x0FFF;
+			break;
+
+
 		//FIXME: THESE EXAMPLES MAY NOT WORK. JUST ADDING THEM FOR NOW.
 
-		case 0x2000:
+		case 0x2000: // 2NNN: Call subroutine
 			chip->STACK[chip->SP] = chip->PC - 2; //FIXME: PC was incremented before this. The code may be wrong.
 			++chip->SP;
 			chip->PC = chip->OPCODE & 0x0FFF;
 			break;
 
-		case 0x0004:       
+		case 0x6000: // 6XKK: VX := KK
+			chip->GPR[(chip->OPCODE & 0x0F00) >> 8] = chip->OPCODE & 0x00FF;
+			break;
+
+		case 0x7000: // 7XKK: VX := VX + KK
+			chip->GPR[(chip->OPCODE & 0x0F00) >> 8] += chip->OPCODE & 0x00FF;
+			break;
+
+		//FIXME: Some of these implementations belong to a greater set
+		// Like this one. It belongs to the class of 8. But only 8XY4 is implemented
+		// Besure to complete these.
+		case 0x0004: // 8XY4: VX := VX + VY, VF := carry
 			if(chip->GPR[(chip->OPCODE & 0x00F0) >> 4] > (0xFF - chip->GPR[(chip->OPCODE & 0x0F00) >> 8]))
 				chip->GPR[0xF] = 1; // carry
 			else
@@ -124,8 +184,8 @@ void emulateCycle(CHIP_8 *chip) {
 			//	chip->PC += 2; //NOTE: Already added this at the start
 			break;
 
-
-		case 0x0033:
+		//FIXME: Another one that belongs to the class of F
+		case 0x0033: // FX33: Store BCD representation of VX in M(I)..M(I+2)
 			chip->MEMORY[chip->Index_REG]     = chip->GPR[(chip->OPCODE & 0x0F00) >> 8] / 100;
 			chip->MEMORY[chip->Index_REG + 1] = (chip->GPR[(chip->OPCODE & 0x0F00) >> 8] / 10) % 10;
 			chip->MEMORY[chip->Index_REG + 2] = (chip->GPR[(chip->OPCODE & 0x0F00) >> 8] % 100) % 10;
@@ -133,6 +193,7 @@ void emulateCycle(CHIP_8 *chip) {
 			break;
 
 		//NOTE: This one handles input
+		//NOTE: Look how this one is handling the E class
 		case 0xE000:
 			switch(chip->OPCODE & 0x00FF)
 			{
@@ -154,6 +215,15 @@ void emulateCycle(CHIP_8 *chip) {
 			}
 
 		//NOTE: This part Handles GRAPHICS
+
+		//TODO: 
+		// [x] 00E0     Clear display 
+		// [x] 1NNN     Jump to NNN 
+		// [x] 6XKK     VX := KK 
+		// [x] 7XKK     VX := VX + KK 
+		// [x] ANNN     I := NNN 
+		// [x] DXYN     Show N-byte sprite from M(I) at coords (VX,VY), VF :=
+		//              collision. If N=0 and extended mode, show 16x16 sprite.
 		case 0xD000:		   
 			{
 				unsigned short x = chip->GPR[(chip->OPCODE & 0x0F00) >> 8];
@@ -181,14 +251,19 @@ void emulateCycle(CHIP_8 *chip) {
 			}
 			break;
 
-			// Update timers
-			if (chip->DELAY_TIMER > 0)
-				--chip->DELAY_TIMER;
 
-			if (chip-> SOUND_TIMER > 0) {
-				if (chip->SOUND_TIMER == 1)
-					printf("BEEP!\n");
-				--chip->SOUND_TIMER;
-			}
 	}
+	
+	// Update timers
+	if (chip->DELAY_TIMER > 0)
+		--chip->DELAY_TIMER;
+
+	if (chip-> SOUND_TIMER > 0) {
+		if (chip->SOUND_TIMER == 1)
+			printf("BEEP!\n");
+		--chip->SOUND_TIMER;
+	}
+
+	printf("PC: %04X OPCODE: %04X\n", chip->PC, chip->OPCODE);
+
 }
