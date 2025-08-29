e552b136ea438092a9d9481976a357ebaf0ded98
Afsan-z47 <ns.afsan@proton.me>
2025-08-29 20:14:56 +0600

    FEAT: Texture based graphics rendering implemented

--- src/display.h
+++ src/display.h
@@ -2,9 +2,11 @@
 #define __DISPLAY__
 #include "chip8.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
 
 extern SDL_Window *window;
 extern SDL_Renderer *renderer;
+extern SDL_Texture *texture;
 
 void init_Graphics();
 void draw_Graphics(CHIP_8 *chip);
